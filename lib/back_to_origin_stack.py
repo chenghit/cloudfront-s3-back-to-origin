19fed0796db63544f44ee97430111e25bef0a063
Cheng Chen <chenghit@qq.com>
2022-10-20 12:22:33 +0000

    solve lambda@edge region issue #5

--- lib/back_to_origin_stack.py
+++ lib/back_to_origin_stack.py
@@ -28,27 +28,10 @@ class BackToOriginStack(Stack):
         
         #gcs_bucket_name = CfnParameter(self, "GcsBucketName", type="String",
         #    description="The name of the Google Cloud Storage bucket.")
-        allowed_region = [
-            'us-east-1',
-            'us-east-2',
-            'us-west-2',
-            'ap-south-1',
-            'ap-northeast-2',
-            'ap-southeast-1',
-            'ap-southeast-2',
-            'ap-northeast-1',
-            'eu-central-1',
-            'eu-west-1',
-            'eu-west-2',
-            'sa-east-1',   
-        ]
         
         gcs_domain_name = 'storage.googleapis.com'
         gcs_bucket_name = 'my_gcp_bucket_9527'
         solution_region = Aws.REGION
-        
-        if not (solution_region in allowed_region):
-            sys.exit('You cannot deploy the solution in {} region.'.format(solution_region))
         
         s3_bucket = s3.Bucket(
             self, "DestinationBucket",
